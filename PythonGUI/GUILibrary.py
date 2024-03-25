c26ce7394bf7d81d202a62a9b66b31b59ac0c476
Gabe <gabealexandratos@gmail.com>
2024-03-25 14:53:15 +1100

    .

--- PythonGUI/GUILibrary.py
+++ PythonGUI/GUILibrary.py
@@ -1,10 +1,20 @@
 from tkinter import *
 from tkinter import messagebox
+import pygame
 
 TITLE = "Game Center"
 
 WINWIDTH = 600
 WINHEIGHT = 400
+
+SWINWIDTH = 300 
+SWINHEIGHT = 300
+
+AWINWIDTH = 300
+AWINHEIGHT = 300
+
+MWINWIDTH = 400
+MWINHEIGHT = 400
 
 window = Tk()
 
@@ -15,15 +25,26 @@ window.resizable(False, False)
 def HelloCallBack():
     msg=messagebox.showinfo("CallBack", "Text")
 
+def OpenMineWindow():
+    mineWindow = Toplevel(window)
+    mineWindow.title("Minesweeper")
+    mineWindow.minsize(MWINWIDTH, MWINHEIGHT)
+    mineWindow.resizable(False, False)
+    Label(mineWindow, text = "this is the text for MineSweeper").pack()
+
 def OpenSnakeWindow():
     snakeWindow = Toplevel(window)
     snakeWindow.title("Snake")
+    snakeWindow.minsize(SWINWIDTH, SWINHEIGHT)
+    snakeWindow.resizable(False, False)
     Label(snakeWindow, text = "this is the text for snake").pack()
 
 def OpenAstroidsWindow():
-    snakeWindow = Toplevel(window)
-    snakeWindow.title("Snake")
-    Label(snakeWindow, text = "this is the text for Astroids").pack()
+    astroidsWindow = Toplevel(window)
+    astroidsWindow.title("Astroids")
+    astroidsWindow.minsize(AWINWIDTH, AWINHEIGHT)
+    astroidsWindow.resizable(False, False)
+    Label(astroidsWindow, text = "this is the text for Astroids").pack()
 
 def TitleSetup(text):
     titleText = Label(window, text=text)
@@ -37,6 +58,10 @@ def SnakeButtonSetup(text, xPos, yPos):
 def AstroidsButtonSetup(text, xPos, yPos):
     buttonOne = Button(window, text=text, height = 2, width = 13, command = OpenAstroidsWindow, bg="silver")
     buttonOne.place(x=xPos, y=yPos)
+
+def MineButtonSetup(text, xPos, yPos):
+    buttonOne = Button(window, text=text, height = 2, width = 13, command = OpenMineWindow, bg="silver")
+    buttonOne.place(x=xPos,y=yPos)
     
 def ButtonSetup(text, xPos, yPos):
     buttonOne = Button(window, text=text, height = 2, width = 13,command = HelloCallBack, bg="silver")
@@ -51,7 +76,7 @@ def DrawMenu():
     
     SnakeButtonSetup("Snake", 100, 100)
     AstroidsButtonSetup("Astroids", 250, 100)
-    ButtonSetup("option3", 400, 100)
+    MineButtonSetup("MineSweeper", 400, 100)
     ButtonSetup("option4", 175, 175)
     ButtonSetup("option4", 325, 175)
     
