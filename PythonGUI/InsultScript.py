e3f0b10e6ea4cb8d24293e7019f50a697271e76b
Gabe <gabealexandratos@gmail.com>
2024-04-02 18:41:23 +1100

    Guessing and Insult finished

---
+++ PythonGUI/InsultScript.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+import pygame
+import random
+
+InsultWindow = Tk()
+
+InsultWindow.title("InsultMe")
+InsultWindow.minsize(300, 300)
+InsultWindow.resizable(False, False)
+
+InsultWindow.mainLoop()
